7e9ad2a512e60af39be40e15f4ed3de196db8639
kominh <kominh@hotmail.com>
2019-08-27 12:32:12 -0400

    updated

--- app.js
+++ app.js
@@ -28,11 +28,12 @@ overlay.addEventListener('click', () => {
 //split the array into new array of characters
 //return new array of characters
 //Math.floor(Math.random() * 5) + 1;
+
 function getRandomPhraseAsArray (gamePhrases) {
  
-randomPhrase = gamePhrases[(Math.floor(Math.random() * gamePhrases.length))];
-newPhrase = randomPhrase.split('');
-return newPhrase
+	randomPhrase = gamePhrases[(Math.floor(Math.random() * gamePhrases.length))];
+	newPhrase = randomPhrase.split('');
+		return newPhrase
 };
 
 
@@ -74,97 +75,88 @@ addPhraseToDisplay(phrases);
 
 
 
-
-
 /****
-//Create a checkLetter function.
+Create a checkLetter function.
 The checkLetter function will be used inside of the event listener 
-//This function should have one parameter: the button the player has 
-//clicked when guessing a letter.
-//The checkLetter function should get all of the
-//elements with a class of “letter” 
-//(remember that we added the letter class to all of the letters and
-// none of the spaces when we made the game display). 
+This function should have one parameter: the button the player has 
+clicked when guessing a letter.
+
+The checkLetter function should get all of the
+elements with a class of “letter” 
+
+The function should loop over the letters and check if they match the 
+letter in the button the player has chosen.
+
+If there’s a match, the function should add the “show” class to the list item
+containing that letter, store the matching letter inside of a variable, and return that letter
+
+If a match wasn’t found, the function should return null
+
+return match
 ***/
-
-
-
-function checkLetter(button) {
+function checkLetter(button) {                
 	
+	const liList = document.querySelectorAll('.letter');
+	let match = null;
+	
+		for (let i = 0; i < liList.length; i += 1) {
 		
-		const liList = document.querySelectorAll('.letter');
-		let match = null;
-	//The function should loop over the letters and check if they match the 
-    //letter in the button the player has chosen.
-	for (let i = 0; i < liList.length; i += 1) {
-
-			//If there’s a match, the function should add the “show” class to the list item
-			//containing that letter, store the matching letter inside of a variable,
-			//and return that letter.
-			if (liList[i].textContent == button) {
-					liList[i].className = 'show';
-					match = button;
-									
+			if (liList[i].textContent.toUpperCase() == button.toUpperCase()) {
+				liList[i].className = ('.show', '.letter');
+				match = button;
 			}
-
-			 //else {//If a match wasn’t found, the function should return null.	
-					// }	
-		} return match;
-		
+		}
+			return match;
 };
 
 
-	const button = document.getElementsByTagName('button');
-	qwerty.addEventListener('click', (event) => {
-
-		
-
-		for (let i = 0; i < button.length; i += 1){
-			
-			
-			if (event.target.tagName == 'BUTTON') {
-				let playerGuess = event.target.innerText;
-
-					if (button[i] == event.target){
-					button[i].className = 'chosen';
-					button[i].setAttribute("disabled", true);
-					const clickButton = button[i].textContent;
-					console.log(clickButton)
-					}		
-				
-						
-				//console.log(playerGuess);
-					}
-
-				}	
-		});
-
-	
-////////////
 /**
 Add an event listener to the keyboard.
-
-
 
 Use event delegation to listen only to button events from the keyboard. 
 
 When a player chooses a letter, add the “chosen” class to that button so
- the same letter can’t be chosen twice. Note that button elements have an
-  attribute you can set called “disabled” that when set to true will not 
-  respond to user clicks. See the MDN documentation for more details.
+the same letter can’t be chosen twice. 
+
+Note that button elements have an
+attribute you can set called “disabled” that when set to true will not 
+respond to user clicks.
 
 Pass the button to the checkLetter function, and store the letter returned 
-inside of a variable called letterFound. At this point, you can open the 
+inside of a variable called 'clickbutton'. 
+
+At this point, you can open the 
 index.html file, click any of the letters on the keyboard, and start to see 
 the letters appear in the phrase.
-
 **/
 
+const button = document.getElementsByTagName('button');
+qwerty.addEventListener('click', (event) => {
+
+	for (let i = 0; i < button.length; i += 1){
+			
+		if (event.target.tagName == 'BUTTON') {
+		
+			if (button[i] == event.target){
+				button[i].className = 'chosen';
+				button[i].setAttribute("disabled", true);
+				
+				const clickButton = button[i].textContent;
+				checkLetter(clickButton);
+					}		
+				}//console.log(playerGuess);
+			}	
+		});
+
+
+	
 
 
 
-//listen for user actions or events
-//example
+
+
+
+
 
 
 
